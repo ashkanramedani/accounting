a2a066c72dbdffd64ea3cbf3fd811ceb2ea00f96
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-05-13 13:14:26 +0330

    Default Roles

--- api/db/models/SetUp.py
+++ api/db/models/SetUp.py
@@ -4,28 +4,46 @@ from lib import logger
 from sqlalchemy.orm import Session
 import db.models as dbm
 
+DEFAULT_ROLES = [
+    {"name": "Manager", "cluster": "Manager"},
+    {"name": "Unknown", "cluster": "Users"},
+    {"name": "Student", "cluster": "Users"},
+    {"name": "Support", "cluster": "Supports"},
+    {"name": "Teacher", "cluster": "Teachers"}
+]
 
 def setUp_admin(db: Session):
     try:
         emp = db.query(dbm.User_form).filter_by(name="Admin").first()
         if not emp:
             UID = uuid.uuid4()
-
-            OBJ = dbm.User_form(user_pk_id=UID, created_fk_by=UID, name="Admin", last_name="Admin", status=1)  # type: ignore[call-arg]
-            db.add(OBJ)
+            admin_user = dbm.User_form(user_pk_id=UID, created_fk_by=UID, name="Admin", last_name="Admin", status=1)  # type: ignore[call-arg]
+            db.add(admin_user)
             db.commit()
-            db.refresh(OBJ)
-            emp = db.query(dbm.User_form).filter_by(name="Admin").first()
+            db.refresh(admin_user)
+            emp = admin_user
 
-            if not db.query(dbm.Role_form).filter_by(name="Administrator").first():
-                OBJ = dbm.Role_form(created_fk_by=emp.user_pk_id, name="Administrator", cluster="Administrator", status=1)  # type: ignore[call-arg]
-                db.add(OBJ)
+            admin_role = db.query(dbm.Role_form).filter_by(name="Administrator").first()
+            if not admin_role:
+                admin_role = dbm.Role_form(created_fk_by=emp.user_pk_id, name="Administrator", cluster="Administrator", status=1)   # type: ignore[call-arg]
+                db.add(admin_role)
                 db.commit()
-                db.refresh(OBJ)
 
-            emp.roles.append(db.query(dbm.Role_form).filter_by(name="Administrator").first())
+            emp.roles.append(admin_role)
+
+        existing_roles = db.query(dbm.Role_form).filter(dbm.Role_form.name.in_([r["name"] for r in DEFAULT_ROLES])).all()
+        existing_role_names = [role.name for role in existing_roles]
+
+        new_roles = []
+        for role_data in DEFAULT_ROLES:
+            if role_data["name"] not in existing_role_names:
+                new_roles.append(dbm.Role_form(created_fk_by=emp.user_pk_id, **role_data, status=1))  # type: ignore[call-arg]
+
+        if new_roles:
+            db.bulk_save_objects(new_roles)
             db.commit()
-        logger.info(f'Admin Setup Finished')
+
+        logger.info('Admin Setup Finished')
     except Exception as e:
         db.rollback()
         logger.error("Admin Setup Failed")
