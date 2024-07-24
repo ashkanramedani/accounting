9a220f4955ba42dbdd326a7dff035987767929df
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-07-24 15:28:34 +0330

    minor changes

--- api/db/Extra/Status.py
+++ api/db/Extra/Status.py
@@ -1,26 +1,34 @@
+
+from lib.decorators import _io
+import db.models as dbm
+from db.models import SessionLocal
 from sqlalchemy.orm import Session
 
-import db.models as dbm
 
-
-def preprocess_keys(*args):
+def preprocess_keys(*args) -> list[str]:
     return [k.lower().strip() for k in args]
 
 
+# @_io()
 def Set_Status(db: Session, cluster: str, status: str) -> str:
     status, cluster = preprocess_keys(status, cluster)
-
     Status_OBJ: dbm.Status_form = db.query(dbm.Status_form).filter_by(status_name=status, status_cluster=cluster).first()
 
     if not Status_OBJ:
-        try:
-            Admin = db.query(dbm.User_form).filter_by(name="Admin", last_name="Admin").first().user_pk_id
-        except AttributeError:
-            raise AttributeError("Admin User Not Found")
-        Status_OBJ = dbm.Status_form(status_name=status, status_cluster=cluster, created_fk_by=Admin)  # type: ignore[call-arg]
+        Status_OBJ = dbm.Status_form(status_name=status, status_cluster=cluster)  # type: ignore[call-arg]
         db.add(Status_OBJ)
         db.commit()
         db.refresh(Status_OBJ)
         return status
 
     return Status_OBJ.status_name
+
+
+if __name__ == '__main__':
+
+    for s, c in [
+        ('active', 'student'),
+        ('active', 'teacher'),
+        ('inactive', 'student'),
+        ('inactive', 'teacher')]:
+        Set_Status(SessionLocal(), s, c)
