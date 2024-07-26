f58dc682b4c6f57a51be58f9af445fa82ef4731d
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-07-26 11:01:40 +0330

    Add append mode for DevIo

--- api/lib/__init__.py
+++ api/lib/__init__.py
@@ -6,4 +6,4 @@ from .functions import Massenger, Tools
 from .hash import Hash
 from .json_handler import json_handler, JSONEncoder
 from .log import logger
-from .decorators import _io
+from .decorators import DEV_io
--- api/lib/decorators.py
+++ api/lib/decorators.py
@@ -1,29 +1,38 @@
 import json
 import os.path
-import pickle
+from datetime import datetime
+from time import perf_counter
 from functools import wraps
 
-from .json_handler import JSONEncoder
+from lib.json_handler import JSONEncoder
+from lib import logger
 
 
-def _io():
+def DEV_io(mode: str = "w"):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            file_path = f"./TmpFiles/{func.__name__}.json"
+            file_path = f"{func.__name__}.json"
             data = {'inputs': {'args': args, 'kwargs': kwargs}, 'output': func(*args, **kwargs)}
-            if os.path.exists("./TmpFiles"):
-                json.dump(data, open(file_path, 'w'), indent=4, cls=JSONEncoder)
-
+            if os.path.exists("DBug_IO"):
+                if mode == 'a':
+                    OBJ = json.load(open(os.path.join("DBug_IO", file_path)))
+                    OBJ[str(datetime.now())] = data
+                    json.dump(OBJ, open(os.path.join("DBug_IO", file_path), 'w'), indent=4, cls=JSONEncoder)
+                elif mode == 'w':
+                    json.dump(data, open(os.path.join("DBug_IO", file_path), 'w'), indent=4, cls=JSONEncoder)
             return data['output']
+
         return wrapper
+
     return decorator
 
 
 if __name__ == '__main__':
-    @_io()
+    @DEV_io()
     def add(a, b):
         return a + b
+
 
     # Call the function
     result = add(3, 5)
