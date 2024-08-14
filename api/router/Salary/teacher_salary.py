f18cd6ad77ae6e5bbd7b19259916964cdf214dfc
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-08-14 17:54:10 +0330

    teacher summary changed to post instead of get and can accept dropdowns

--- api/router/Salary/teacher_salary.py
+++ api/router/Salary/teacher_salary.py
@@ -1,5 +1,6 @@
 from .Base import *
 
+
 # Form: sch.teacher_salary_report
 
 @router.get("/teacher/courses", dependencies=[Depends(RateLimiter(times=1000, seconds=1))], response_model=List[sch.Teacher_course_report])
@@ -18,9 +19,9 @@ async def teacher_sub_course(course_id: UUID, db=Depends(get_db)):
     return result
 
 
-@router.get("/teacher/summary/{course_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))])
-async def teacher_summary(course_id: UUID, db=Depends(get_db)):
-    status_code, result = dbf.course_report_summary(db, course_id, 0)
+@router.post("/teacher/summary/{course_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))])
+async def teacher_summary(course_id: UUID, Dropdowns: sch.teacher_salary_DropDowns, db=Depends(get_db)):
+    status_code, result = dbf.SubCourse_report(db, course_id, Dropdowns)
     if status_code not in sch.SUCCESS_STATUS:
         raise HTTPException(status_code=status_code, detail=result)
     return result
