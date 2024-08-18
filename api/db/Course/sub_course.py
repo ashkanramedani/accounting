5a7837eaccc019b574d466ea5ad84aeb29572fd0
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-08-18 21:35:37 +0330

    optimizig the code and creating the sesssion

--- api/db/Course/sub_course.py
+++ api/db/Course/sub_course.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-from typing import List
+from typing import List, Dict
 from uuid import UUID
 
 from sqlalchemy.orm import Session
@@ -51,6 +51,86 @@ def get_all_subcourse(db: Session, page: sch.NonNegativeInt, limit: sch.Positive
 
 def post_subcourse(db: Session, Form: sch.post_sub_course_schema):
     try:
+        # Validate employee and course existence
+        employee_exists = employee_exist(db, [Form.created_fk_by, Form.sub_course_teacher_fk_id])
+        if not employee_exists:
+            return 400, "Bad Request: employee not found"
+
+        course: dbm.Course_form = db.query(dbm.Course_form).filter_by(course_pk_id=Form.course_fk_id).filter(dbm.Course_form.status != "deleted").first()
+        if not course:
+            return 400, "Bad Request: course not found"
+
+        data = Form.__dict__
+        session_signature = data.pop("session_signature")
+        # Create SubCourse
+        sub_course: dbm.Sub_Course_form = create_subcourse(db, data, course)
+
+        # Create Sessions
+        if session_signature:
+            duplicates = create_sessions(db, Form, sub_course, session_signature, [f'{session.session_date} {session.session_starting_time}' for session in (course.sessions if course.sessions else [])])
+            MSG = f"SubCourse Added ( {len(sub_course.sessions)}/{Form.number_of_session} SESSION ) {duplicates}"
+        else:
+            MSG = "Empty SubCourse Added ( NO SESSION )"
+
+        db.commit()
+        return 200, MSG
+    except Exception as e:
+        db.rollback()
+        return Return_Exception(db, e)
+
+
+def create_subcourse(db: Session, sub_course_data: Dict, course) -> dbm.Sub_Course_form:
+    course_data = {"sub_course_capacity": course.course_capacity, "sub_course_available_seat": course.course_capacity}
+    sub_course = dbm.Sub_Course_form(**sub_course_data, **course_data)  # type: ignore
+    db.add(sub_course)
+    db.flush()
+    return sub_course
+
+
+def create_sessions(db: Session, Form: sch.post_sub_course_schema, sub_course: dbm.Sub_Course_form, session_signature: dict, existing_sessions: List[str]):
+    Session_signature = {}
+    sub_request_threshold = Form.sub_request_threshold
+
+    for i in session_signature:
+        if i.days_of_week not in Session_signature:
+            Session_signature[i.days_of_week] = []
+        Session_signature[i.days_of_week].append((i.starting_time, i.duration))
+
+    date_table = generate_time_table(
+            starting_date=Form.sub_course_starting_date,
+            ending_date=Fix_date(Form.sub_course_ending_date),
+            day_of_week=Session_signature.keys())
+
+    WARN = []
+    for day_date, day_weekday in date_table[:Form.number_of_session]:
+        for start_time, session_duration in Session_signature[day_weekday]:
+            session_starting_time = datetime.combine(day_date, Fix_time(start_time))
+
+            SIG = f'{session_starting_time}'
+            if SIG in existing_sessions:
+                WARN.append(f"{SIG} - Already Exist")
+                continue
+
+            session_ending_time = session_starting_time + timedelta(minutes=session_duration)
+            can_accept_sub = datetime.combine(day_date, session_starting_time.time()) - timedelta(hours=sub_request_threshold)
+
+            session_data = {
+                "created_fk_by": Form.created_fk_by,
+                "course_fk_id": Form.course_fk_id,
+                "sub_course_fk_id": sub_course.sub_course_pk_id,
+                "session_teacher_fk_id": Form.sub_course_teacher_fk_id,
+                "session_date": day_date,
+                "session_starting_time": session_starting_time.time(),
+                "session_ending_time": session_ending_time.time(),
+                "session_duration": session_duration,
+                "days_of_week": day_weekday,
+                "can_accept_sub": can_accept_sub}
+            db.add(dbm.Session_form(**session_data))  # type: ignore[call-arg]
+    db.flush()
+    return WARN
+
+def post_subcourse_back(db: Session, Form: sch.post_sub_course_schema):
+    try:
         if not employee_exist(db, [Form.created_fk_by, Form.sub_course_teacher_fk_id]):
             return 400, "Bad Request: employee not found"
 
@@ -75,38 +155,6 @@ def post_subcourse(db: Session, Form: sch.post_sub_course_schema):
         if not session_signature:
             return 200, "Empty SubCourse Added ( NO SESSION )"
 
-        Session_signature = {}
-        for i in session_signature:
-            if i.days_of_week not in Session_signature:
-                Session_signature[i.days_of_week] = []
-            Session_signature[i.days_of_week].append((i.starting_time, i.duration))
-
-        date_table = generate_time_table(
-                starting_date=Form.sub_course_starting_date,
-                ending_date=Fix_date(Form.sub_course_ending_date),
-                day_of_week=Session_signature.keys())
-
-        days = []
-        for day_date, day_weekday in date_table[:data["number_of_session"]]:
-            for start_time, session_duration in Session_signature[day_weekday]:
-                session_starting_time = datetime.combine(day_date, Fix_time(start_time))
-                session_ending_time = session_starting_time + timedelta(minutes=session_duration)
-                can_accept_sub = datetime.combine(day_date, session_starting_time.time()) - timedelta(hours=sub_request_threshold)
-
-                session_data = {
-                    "created_fk_by": Form.created_fk_by,
-                    "course_fk_id": Form.course_fk_id,
-                    "sub_course_fk_id": OBJ.sub_course_pk_id,
-                    "session_teacher_fk_id": Form.sub_course_teacher_fk_id,
-                    "session_date": day_date,
-                    "session_starting_time": session_starting_time.time(),
-                    "session_ending_time": session_ending_time.time(),
-                    "session_duration": session_duration,
-                    "days_of_week": day_weekday,
-                    "can_accept_sub": can_accept_sub}
-
-                days.append(dbm.Session_form(**session_data))  # type: ignore[call-arg]
-        db.add_all(days)
         db.commit()
         return 200, f"SubCourse Added ( {len(days)}/{data['number_of_session']} SESSION )"
     except Exception as e:
