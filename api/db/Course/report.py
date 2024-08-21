14e36947217d73c3f6a432f78f77cdd8e43dbdac
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-08-21 17:23:59 +0330

    update formula and schema

--- api/db/Course/report.py
+++ api/db/Course/report.py
@@ -9,19 +9,20 @@ from db.Extra import *
 from lib import DEV_io, logger
 from schemas import course_data_for_report
 
-Base_salary = {Cap: {"OnSite": 77, "online": 66, "hybrid": 0} for Cap in ["1-5", "6-9", "10-12", "13"]}
-TEACHER_TARDY = {"0_10": 11, "10_30": 5.5, "30_40": -5.5, "40": -16.5}
-COURSE_LEVEL = {"connect": 11, "FCE": 22, "CAE": 44, "CPE": 66}
+Base_salary = {Cap: {"OnSite": 770_000, "online": 660_000, "hybrid": 0} for Cap in ["1-5", "6-9", "10-12", "13"]}
+TEACHER_TARDY = {"0_10": 110_000, "10_30": 55_000, "30_40": -55_000, "40": -165_000}
+COURSE_LEVEL = {"connect": 110_000, "FCE": 220_000, "CAE": 440_000, "CPE": 660_000}
 SCORES = {
     "effect_on_total": ["loan", "reward", "punishment", "cancelled_session_score"],
-    "effect_on_session": ["content_creation", "event_participate", "CPD", "Odd_hours", "report_to_student", "LP_submission", "student_assign_feedback", "survey_score", "result_submission_to_FD", "roles_score", "tardy_score", "course_level_score"]
+    "Percent_on_session": ["content_creation", "event_participate", "CPD", "Odd_hours"],
+    "effect_on_session": ["report_to_student", "LP_submission", "student_assign_feedback", "survey_score", "result_submission_to_FD", "roles_score", "tardy_score", "course_level_score"]
 }
 DropDown_value_table = {
-    "report_to_student": {"weak": -5.5, "average": 0, "good": 5.5, "excellent": 11},
-    "LP_submission": {"weak": -11, "average": 5.5, "good": 11, "excellent": 16.5},
-    "student_assign_feedback": {"weak": -5.5, "average": 0, "good": 5.5, "excellent": 11},
-    "survey_score": {"weak": 0, "average": 3, "good": 6, "excellent": 10},
-    "result_submission_to_FD": {"weak": -5.5, "average": 0, "good": 5.5}
+    "report_to_student": {"weak": -55_000, "average": 0, "good": 55_000, "excellent": 110_000},
+    "LP_submission": {"weak": -110_000, "average": 55_000, "good": 110_000, "excellent": 165_000},
+    "student_assign_feedback": {"weak": -55_000, "average": 0, "good": 55_000, "excellent": 110_000},
+    "survey_score": {"weak": 0, "average": 30_000, "good": 60_000, "excellent": 100_000},
+    "result_submission_to_FD": {"weak": -55_000, "average": 0, "good": 55_000}
 }
 
 
@@ -63,18 +64,15 @@ def PreProcess_teacher_report(db: Session, sub_course_obj: dbm.Sub_Course_form) 
         for teacher in teachers_query
     }
 
-def Apply_scores(
-        DropDowns: sch.teacher_salary_DropDowns,
-        sub_course_summary: dict[str, sch.Report],
-        course_level: str,
-        subcourse_teacher: UUID) -> List:
 
+def Apply_scores(DropDowns: sch.teacher_salary_DropDowns, sub_course_summary: dict[str, sch.Report], course_level: str, subcourse_teacher: UUID) -> List[Dict]:
     Final = []
     Score = {
         field: value
-        if isinstance(value, float) else DropDown_value_table[field][value.value]
+        if (isinstance(value, float) or isinstance(value, int)) else DropDown_value_table[field][value.value]
         for field, value in DropDowns
     }
+
     Score["course_level_score"] = COURSE_LEVEL.get(course_level, 0)
 
     data = sub_course_summary[str(subcourse_teacher)]
@@ -82,22 +80,21 @@ def Apply_scores(
     Score["cancellation_factor"] *= data.cancelled_session
 
     effect_on_session = sum([value for key, value in Score.items() if key in SCORES["effect_on_session"]])
+    percent_on_session = {key: value for key, value in Score.items() if key in SCORES["Percent_on_session"]}
 
     for teacher_id, data in sub_course_summary.items():
         Total_session = data.attended_session + data.sub_point
         data.score = effect_on_session + data.roles_score
         data.earning = Total_session * effect_on_session
-        percentage = min(100, (data.ID_Experience // 36000) * 2)
-        data.earning += ((data.earning * percentage) / 100)
+        data.ID_Experience = min(100, (data.ID_Experience // 36_000) * 2)
+        data.earning += ((data.earning * data.ID_Experience) / 100)
 
         Final.append({**data.__dict__, **Score, "user_fk_id": teacher_id})
-
-    # Final["score"] = Score
     return Final
 
 
 @DEV_io()
-def SubCourse_report(db: Session, sub_course_id: UUID, DropDowns: sch.teacher_salary_DropDowns) -> Tuple[int, List[dbm.Teacher_salary_form] | str]:
+def SubCourse_report(db: Session, sub_course_id: UUID, DropDowns: sch.teacher_salary_DropDowns):
     sub_course: dbm.Sub_Course_form = db.query(dbm.Sub_Course_form).filter_by(sub_course_pk_id=sub_course_id).first()
 
     if not sub_course:
@@ -106,8 +103,6 @@ def SubCourse_report(db: Session, sub_course_id: UUID, DropDowns: sch.teacher_sa
     Existing_salary = db.query(dbm.Teacher_salary_form).filter_by(subcourse_fk_id=sub_course.sub_course_pk_id).all()
     if Existing_salary:
         return 200, Existing_salary
-
-
 
     course_data = db.query(dbm.Course_form) \
         .filter_by(course_pk_id=sub_course.course_fk_id) \
@@ -130,7 +125,9 @@ def SubCourse_report(db: Session, sub_course_id: UUID, DropDowns: sch.teacher_sa
         return status, sub_course_summary
 
     # Loop Through Sessions
-    for session in db.query(dbm.Session_form).filter_by(sub_course_fk_id=sub_course.sub_course_pk_id).filter(dbm.Session_form.status != "deleted").all():
+    AllSessions = db.query(dbm.Session_form).filter_by(sub_course_fk_id=sub_course.sub_course_pk_id).filter(dbm.Session_form.status != "deleted").all()
+    sub_course_summary[str(sub_course.sub_course_teacher_fk_id)].total_sessions = len(AllSessions)
+    for session in AllSessions:
         Session_teacher = str(session.session_teacher_fk_id)
         if Session_teacher not in sub_course_summary:
             logger.warning(f"Teacher: {Session_teacher} Has Been Skipped. ")
@@ -161,33 +158,46 @@ def SubCourse_report(db: Session, sub_course_id: UUID, DropDowns: sch.teacher_sa
     for tardy in Tardies:
         sub_course_summary[str(tardy.teacher_fk_id)].tardy += tardy.delay
 
-    OBJs = []
+    OBJs: List[dbm.Teacher_salary_form] = []
     teacher_salary_records = Apply_scores(DropDowns, sub_course_summary, course_data.course_level, sub_course.sub_course_teacher_fk_id)
+
+    Extra = {"subcourse_fk_id": sub_course.sub_course_pk_id, "BaseSalary": course_data.BaseSalary, "course_data": course_data.__dict__}
     for salary_record in teacher_salary_records:
-        OBJs.append(dbm.Teacher_salary_form(**salary_record, subcourse_fk_id=sub_course.sub_course_pk_id, BaseSalary=course_data.BaseSalary))  # type: ignore[call-arg]
+        OBJ = dbm.Teacher_salary_form(**salary_record, **Extra)  # type: ignore[call-arg]
+
         User_obj = db.query(dbm.User_form).filter_by(user_pk_id=salary_record["user_fk_id"]).first()
-        User_obj.ID_Experience += salary_record["experience_gain"]
+        User_obj.ID_Experience += salary_record["experience_gain"] + 10_000_000
+        OBJs.append(OBJ)
 
     db.add_all(OBJs)
     db.commit()
-
-    return 200, db.query(dbm.Teacher_salary_form).filter_by(subcourse_fk_id=sub_course.sub_course_pk_id).all()
+    # return 200, OBJs
+    return 200, db \
+        .query(dbm.Teacher_salary_form) \
+        .filter_by(subcourse_fk_id=sub_course.sub_course_pk_id) \
+        .options(
+            joinedload(dbm.Teacher_salary_form.teacher),
+            joinedload(dbm.Teacher_salary_form.card),
+            joinedload(dbm.Teacher_salary_form.sub_course)) \
+        .all()
 
 
 def update_SubCourse_report(db: Session, form_ID: UUID, Form: sch.update_salary_report):
-    Salary_record = db \
-        .query(dbm.Teacher_salary_form) \
-        .filter_by(teacher_salary_pk_id=form_ID) \
-        .filter(dbm.Teacher_salary_form.status != "deleted") \
-        .first()
+    try:
+        existing = db \
+            .query(dbm.Teacher_salary_form) \
+            .filter_by(teacher_salary_pk_id=form_ID) \
+            .filter(dbm.Teacher_salary_form.status != "deleted")
 
-    Salary_record.rewards_earning = Form.rewards_earning
-    Salary_record.punishment_deductions = Form.punishment_deductions
-    Salary_record.loan_installment = Form.loan_installment
+        salary_data = existing.first()
+        if not salary_data:
+            return 400, "Bad Request: Target salary record not found"
 
-    Salary_record.earning += (Form.rewards_earning - Form.punishment_deductions - Form.loan_installment)
+        changes = {**Form.__dict__, "earning": salary_data.earning + (Form.rewards_earning - Form.punishment_deductions - Form.loan_installment)}
 
-    Salary_record.payment = Form.payment
-    Salary_record.payment_date = Form.payment_date
-    db.commit()
-    return 200
+        existing.update({**changes}, synchronize_session=False)
+        db.commit()
+        return 200, existing.first()
+
+    except Exception as e:
+        return Return_Exception(db, e)
