837a894a4d6590b9ca092503c5693554664113c5
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-09-01 18:43:04 +0330

    add supervisor_review route

--- api/router/Salary/teacher_salary.py
+++ api/router/Salary/teacher_salary.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 from lib import logger
 from .Base import *
 
@@ -28,17 +30,33 @@ async def teacher_sub_course(course_id: UUID, db=Depends(get_db)):
     return result
 
 
-@router.post("/teacher/summary/{sub_course_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))]) #, response_model=sch.Teacher_report_response)
+@router.post("/teacher/summary/{sub_course_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))])
 async def teacher_summary(sub_course_id: UUID, Dropdowns: sch.teacher_salary_DropDowns, db=Depends(get_db)):
-    status_code, result = dbf.SubCourse_report(db, sub_course_id, Dropdowns)
+    status_code, result = dbf.SubCourse_report(db, sub_course_id)
     if status_code not in sch.SUCCESS_STATUS:
         raise HTTPException(status_code=status_code, detail=result)
     return result
 
 
-@router.put("/teacher/summary/{form_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))]) #, response_model=sch.Teacher_report_response)
+@router.put("/teacher/summary/{form_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))])
 async def teacher_summary(form_id: UUID, Form: sch.update_salary_report, db=Depends(get_db)):
     status_code, result = dbf.update_SubCourse_report(db, form_id, Form)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
+
+
+@router.get("/teacher/supervisor_review/{sub_course_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))], response_model=Dict)
+async def get_supervisor_review(sub_course_id: UUID, db=Depends(get_db)):
+    status_code, result = dbf.get_supervisor_review(db, sub_course_id)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
+
+
+@router.post("/teacher/supervisor_review/{sub_course_id}", dependencies=[Depends(RateLimiter(times=1000, seconds=1))])  #, response_model=sch.Teacher_report_response)
+async def post_supervisor_review(sub_course_id: UUID, Dropdowns: sch.teacher_salary_DropDowns, db=Depends(get_db)):
+    status_code, result = dbf.post_supervisor_review(db, sub_course_id, Dropdowns)
     if status_code not in sch.SUCCESS_STATUS:
         raise HTTPException(status_code=status_code, detail=result)
     return result
