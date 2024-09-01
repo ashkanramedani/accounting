7b50e8a5991e3c65dc4d0dc6210a07d6db9932a6
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-09-01 18:32:28 +0330

    minor spelling (dropdown)

--- api/router/dropdown.py
+++ api/router/dropdown.py
@@ -5,13 +5,13 @@ from fastapi_limiter.depends import RateLimiter
 
 import db as dbf
 import schemas as sch
-from db.models import get_db
+from models import get_db
 
-router = APIRouter(prefix='/api/v1/deopdown', tags=['Drop Down'])
+router = APIRouter(prefix='/api/v1/dropdown', tags=['Drop Down'])
 
 
 @router.get("/user", dependencies=[Depends(RateLimiter(times=1000, seconds=1))], response_model=List[sch.user_dropdown])
-async def employee_deopdown(db=Depends(get_db), order: sch.Sort_Order = "desc", SortKey: str = "name", employee: bool = True):
+async def employee_dropdown(db=Depends(get_db), order: sch.Sort_Order = "desc", SortKey: str = "name", employee: bool = True):
     status_code, result = dbf.user_dropdown(db, order, SortKey, is_employee=employee)
     if status_code not in sch.SUCCESS_STATUS:
         raise HTTPException(status_code=status_code, detail=result)
