a00ceac7b6e473da39fd5deda997530d0198e2b0
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-09-11 20:46:39 +0330

    add Status schema

---
+++ api/schemas/status.py
@@ -0,0 +1,26 @@
+from schemas.Base import *
+
+class Status(Base_form):
+    status_name: str
+    status_cluster: str
+
+class post_status_schema(Status):
+    pass
+
+    class Config:
+        extra = 'ignore'
+
+class update_status_schema(Status):
+    status_pk_id: UUID
+
+    class Config:
+        extra = 'ignore'
+
+class status_response(BaseModel):
+    status_pk_id: UUID
+    status_name: str
+    status_cluster: str
+
+    class Config:
+        orm_mode = True
+        extra = 'ignore'
