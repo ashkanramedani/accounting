4b168a52fc8ad522d354d7b3da894e9cebcc00b7
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-09-18 17:17:18 +0330

    minor code change

--- api/db/Extra/Test.py
+++ api/db/Extra/Test.py
@@ -4,5 +4,5 @@
 from sqlalchemy.orm import Session
 
 
-def TestRoute(db: Session, created_by="00000000-0000-4b94-8e27-44833c2b940f", user_id="00000001-0000-4b94-8e27-44833c2b940f"):
+def TestRoute(db: Session, **kwargs):
     pass
