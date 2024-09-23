06fef5b3ef8de1cdc4dabc50e6781dbe861f68aa
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-09-23 16:09:21 +0330

    minor code improvement related to year convertion

--- api/lib/Date_Time.py
+++ api/lib/Date_Time.py
@@ -267,34 +267,37 @@ def Separate_days(start, end):
 
 
 def to_persian(year, month, day, return_obj=True) -> tuple | date:
-    d_4 = year % 4
-    g_a = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
-    doy_g = g_a[month] + day
-    if d_4 == 0 and month > 2:
-        doy_g += 1
-    d_33 = int(((year - 16) % 132) * .0305)
-    a = 286 if (d_33 == 3 or d_33 < (d_4 - 1) or d_4 == 0) else 287
-    if (d_33 == 1 or d_33 == 2) and (d_33 == d_4 or d_4 == 1):
-        b = 78
+    if year < 2000:
+        jy, jm, jd = year, month, day
     else:
-        b = 80 if (d_33 == 3 and d_4 == 0) else 79
-    if int((year - 10) / 63) == 30:
-        a -= 1
-        b += 1
-    if doy_g > b:
-        jy = year - 621
-        doy_j = doy_g - b
-    else:
-        jy = year - 622
-        doy_j = doy_g + a
-    if doy_j < 187:
-        jm = int((doy_j - 1) / 31)
-        jd = doy_j - (31 * jm)
-        jm += 1
-    else:
-        jm = int((doy_j - 187) / 30)
-        jd = doy_j - 186 - (jm * 30)
-        jm += 7
+        d_4 = year % 4
+        g_a = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+        doy_g = g_a[month] + day
+        if d_4 == 0 and month > 2:
+            doy_g += 1
+        d_33 = int(((year - 16) % 132) * .0305)
+        a = 286 if (d_33 == 3 or d_33 < (d_4 - 1) or d_4 == 0) else 287
+        if (d_33 == 1 or d_33 == 2) and (d_33 == d_4 or d_4 == 1):
+            b = 78
+        else:
+            b = 80 if (d_33 == 3 and d_4 == 0) else 79
+        if int((year - 10) / 63) == 30:
+            a -= 1
+            b += 1
+        if doy_g > b:
+            jy = year - 621
+            doy_j = doy_g - b
+        else:
+            jy = year - 622
+            doy_j = doy_g + a
+        if doy_j < 187:
+            jm = int((doy_j - 1) / 31)
+            jd = doy_j - (31 * jm)
+            jm += 1
+        else:
+            jm = int((doy_j - 187) / 30)
+            jd = doy_j - 186 - (jm * 30)
+            jm += 7
 
     if return_obj:
         return date(jy, jm, jd)
@@ -302,19 +305,20 @@ def to_persian(year, month, day, return_obj=True) -> tuple | date:
 
 
 def to_international(year, month, day, return_obj=True) -> tuple | date:
-    d_4 = (year + 1) % 4
-    doy_j = ((month - 1) * 31 + day) if month < 7 else ((month - 7) * 30 + day + 186)
-    d_33 = int(((year - 55) % 132) * .0305)
-    a = 287 if (d_33 != 3 and d_4 <= d_33) else 286
-    b = 78 if (d_33 == 1 or d_33 == 2) and (d_33 == d_4 or d_4 == 1) else (80 if (d_33 == 3 and d_4 == 0) else 79)
-    a -= 1 if int((year - 19) / 63) == 20 else 0
-    b += 1 if int((year - 19) / 63) == 20 else 0
-    year, day = (year + 621, doy_j + b) if doy_j <= a else (year + 622, doy_j - a)
+    if year > 2000:
+        d_4 = (year + 1) % 4
+        doy_j = ((month - 1) * 31 + day) if month < 7 else ((month - 7) * 30 + day + 186)
+        d_33 = int(((year - 55) % 132) * .0305)
+        a = 287 if (d_33 != 3 and d_4 <= d_33) else 286
+        b = 78 if (d_33 == 1 or d_33 == 2) and (d_33 == d_4 or d_4 == 1) else (80 if (d_33 == 3 and d_4 == 0) else 79)
+        a -= 1 if int((year - 19) / 63) == 20 else 0
+        b += 1 if int((year - 19) / 63) == 20 else 0
+        year, day = (year + 621, doy_j + b) if doy_j <= a else (year + 622, doy_j - a)
 
-    for month, v in enumerate([0, 31, 29 if (year % 4 == 0) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]):
-        if day <= v:
-            break
-        day -= v
+        for month, v in enumerate([0, 31, 29 if (year % 4 == 0) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]):
+            if day <= v:
+                break
+            day -= v
 
     if return_obj:
         return date(year, month, day)
