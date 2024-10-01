134108afd1322ada03260818307cf326aa4aeed3
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-01 13:20:04 +0330

    fix bug related to Engine instance created multiple time

--- api/models/Dependency.py
+++ api/models/Dependency.py
@@ -1,9 +1,20 @@
 # Dependency
 from sqlalchemy.orm import Session, sessionmaker
-
 from models import Create_engine
-engine = Create_engine()
+
+
+class Engine:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = Create_engine()
+        return cls._instance
+
+
+engine = Engine()
 SESSION = sessionmaker(autoflush=False, bind=engine)
+
 
 def get_db() -> Session:
     db = SESSION()
