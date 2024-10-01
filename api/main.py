80647f577ca8100e4ed1355458edab3b9f7b8b96
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-01 13:27:16 +0330

    finishing touch on Access middleware

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+print(f'\n{" -.-" * 11}\n\t\tStarting API\n{" -.-" * 11}')
+
 try:
     from os import getenv
     from typing import List
@@ -9,52 +11,57 @@ try:
 
     # DB
     from redis import asyncio as redis
+    from sqlalchemy.orm import sessionmaker
 
     # fastApi
     from fastapi import FastAPI, Request
     from fastapi_limiter import FastAPILimiter
     from contextlib import asynccontextmanager
     from fastapi.middleware.cors import CORSMiddleware
+    from starlette.responses import Response
 
 except (ImportError, ModuleNotFoundError):
     raise Exception('Requirement Not Satisfied: some_module is missing')
 
-from router import routes
-from lib import logger
-from db import save_route
-from models import SetUp, Create_engine, Create_Redis_URL, sessionmaker, SetUp_table
+try:
+    from db import save_route
+    from lib import logger, access_log
+    from models import SetUp, Create_Redis_URL, SetUp_table
+    from router import routes
+    from typing import Dict
+    from models.Dependency import engine
 
-print(f'{" -.-" * 10}\n\t\tStarting API\n{" -.-" * 10}')
+except ImportError as e:
+    raise ImportError(f'{e.__class__.__name__}: {e.__repr__()}')
 
-config = load(open("configs/config.json"))
-IRAN_TIMEZONE = timezone(offset=timedelta(hours=3, minutes=30))
+load_dotenv()
+
+config: Dict = load(open("configs/config.json"))
+IRAN_TIMEZONE: timezone = timezone(offset=timedelta(hours=3, minutes=30))
 
 
 @asynccontextmanager
 async def app_lifespan(api):
-    try:
-        logger.info(f"preparing {api.title} V: {api.version} - {datetime.now(tz=IRAN_TIMEZONE)}")
-        engine = Create_engine(config.get("db", None))
-        res = SetUp_table(engine)
-        if not res:
-            logger.error("Database Setup failed")
-            raise Exception
+    logger.info(f"preparing {api.title} V: {api.version} - {datetime.now(tz=IRAN_TIMEZONE)}")
+    res = SetUp_table(engine)
+    if not res:
+        logger.error("Database Setup failed")
+        raise Exception
 
-        if not getenv('MODE') == "DEBUG":
-            with sessionmaker(autoflush=False, bind=engine)() as Tmp_Connection:
-                SetUp(Tmp_Connection)
+    if not getenv('MODE') == "DEBUG":
+        with sessionmaker(autoflush=False, bind=engine)() as Tmp_Connection:
+            SetUp(Tmp_Connection)
 
-        redis_obj = redis.from_url(Create_Redis_URL(config.get("redis", None)), encoding="utf8")
-        await FastAPILimiter.init(redis=redis_obj)
+    redis_obj = redis.from_url(Create_Redis_URL(config.get("redis", None)), encoding="utf8")
+    await FastAPILimiter.init(redis=redis_obj)
 
-        logger.info(f'{api.title} V: {api.version} Has been started ...')
-        yield
-    finally:
-        logger.info(f'Exiting from {api.title} V: {api.version} - {datetime.now(tz=IRAN_TIMEZONE)}')
-        await FastAPILimiter.close()
+    logger.info(f'{api.title} V: {api.version} Has been started ...')
+    yield
+
+    logger.info(f'Exiting from {api.title} V: {api.version} - {datetime.now(tz=IRAN_TIMEZONE)}')
+    await FastAPILimiter.close()
 
 
-load_dotenv()
 app = FastAPI(
         lifespan=app_lifespan,
         version=config["versions"],
@@ -84,17 +91,27 @@ METHOD = {
 
 
 @app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    # start_time = time()
+async def Access(request: Request, call_next):
+    start_time = time()
 
-    response = await call_next(request)
-    # try:
-    #     logger.log("INFO_access", f"[ {time() - start_time:.3f}s ] {response.status_code} / {METHOD[request.method]: >3} - {request.url}")
-    # except ValueError:
-    #     pass
+    try:
+        response = await call_next(request)
+        body = b"".join([chunk async for chunk in response.body_iterator])
 
-    # response.headers["X-Process-Time"] = f'{time() - start_time:.5f}'
+        MSG = body.decode()
+        MSG = None if "<!DOCTYPE html>" in MSG else MSG
+
+        response = Response(content=body, status_code=response.status_code, headers=dict(response.headers))
+
+    except Exception as e:
+        MSG = f"Error in parsing: {e.__class__.__name__} - {e.args}"
+        return Response(content=f"Error processing request: {MSG}", status_code=500)
+
+    end_time = time()
+    access_log.info(f"[ {end_time - start_time:.3f}s ] - {request.method} - {request.url}", data=MSG)
+
     return response
+
 
 if getenv('CREATE_ROUTE_SCHEMA'):
     logger.info('Creating Route Schema')
