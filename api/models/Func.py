d2208cccd298ea982d81d6bc757a5339d0c66d3d
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-01 13:20:52 +0330

    remove ondelete="CASCADE"

--- api/models/Func.py
+++ api/models/Func.py
@@ -1,9 +1,8 @@
 from fastapi_utils.guid_type import GUID as GUID_TYPE, GUID_SERVER_DEFAULT_POSTGRESQL
-from sqlalchemy import Column, ForeignKey, MetaData
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey
 from datetime import datetime, timedelta, timezone
 
-metadata_obj = MetaData()
+BASE_ATTR = ["created_fk_by", "_sa_instance_state", "priority", "visible", "deleted", "can_update", "can_deleted", "create_date", "update_date", "delete_date", "expire_date", "status", "description", "note"]
 
 
 class GUID(GUID_TYPE):
@@ -11,28 +10,25 @@ class GUID(GUID_TYPE):
 
 
 def create_Unique_ID():
-    return Column(GUID, server_default=GUID_SERVER_DEFAULT_POSTGRESQL, primary_key=True, nullable=False, unique=True, index=True)
+    return Column(
+            GUID,
+            server_default=GUID_SERVER_DEFAULT_POSTGRESQL,
+            primary_key=True,
+            nullable=False,
+            unique=True,
+            index=True)
 
 
 def create_foreignKey(table: str, unique: bool = False, index: bool = True, nullable: bool = False):
     table_name = table.lower().replace("_form", "")
-    return Column(GUID, ForeignKey(f'{table_name}.{table_name + "_pk_id"}', ondelete="CASCADE"), nullable=nullable, unique=unique, index=index)
-
-
-def creator_relation(table: str):
-    """
-    Create a relationship with creator. (Did not used in new version)
-    args:
-        table: table name
-    """
-    return relationship(table, back_populates="created", foreign_keys=f"{table}.created_fk_by", cascade="all, delete-orphan")
+    return Column(GUID, ForeignKey(f'{table_name}.{table_name + "_pk_id"}'), nullable=nullable, unique=unique, index=index)
 
 
 def Remove_Base_Data(OBJ) -> str:
-    for i in ["created_fk_by", "_sa_instance_state", "priority", "visible", "deleted", "can_update", "can_deleted", "create_date", "update_date", "delete_date", "expire_date", "status", "description", "note"]:
-        if i in OBJ:
-            OBJ.pop(i)
+    for i in BASE_ATTR:
+        OBJ.pop(i, None)
     return repr(OBJ)
+
 
 def IRAN_TIME():
     return datetime.now(tz=timezone(offset=timedelta(hours=3, minutes=30)))
