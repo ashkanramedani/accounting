4d5ead8286f91cac48c8f68d93849c0b30b37124
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-14 10:50:51 +0330

    creating Unique id for default record has been updated

--- api/models/Func.py
+++ api/models/Func.py
@@ -1,27 +1,42 @@
-import functools
 import re
-from collections.abc import Callable
-from lib import logger
-from typing import Dict
-from fastapi_utils.guid_type import GUID as GUID_TYPE, GUID_SERVER_DEFAULT_POSTGRESQL
-from sqlalchemy import Column, ForeignKey
+import uuid
 from datetime import datetime, timedelta, timezone
+from typing import Dict, no_type_check
+from uuid import UUID
 
-BASE_ATTR = ["created_fk_by", "_sa_instance_state", "priority", "visible", "deleted", "can_update", "can_deleted", "create_date", "update_date", "delete_date", "expire_date", "status", "description", "note"]
+from fastapi_utils.guid_type import GUID_SERVER_DEFAULT_POSTGRESQL as UNIQUE_ID, UUIDTypeDecorator, GUID as GTYPE
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import CHAR
 
+BASE_ATTR = [
+    "created_fk_by",
+    "_sa_instance_state",
+    "priority",
+    "visible",
+    "deleted",
+    "can_update",
+    "can_deleted",
+    "create_date",
+    "update_date",
+    "expire_date",
+    "status",
+    "description",
+    "note"]
 
-class GUID(GUID_TYPE):
+CODES = {
+    "user": "0",
+    "role": "1",
+    "status": "2",
+    "language": "3",
+    "course_type": "4"
+}
+
+class GUID(GTYPE):
     cache_ok = True
 
 
 def create_Unique_ID():
-    return Column(
-            GUID,
-            server_default=GUID_SERVER_DEFAULT_POSTGRESQL,
-            primary_key=True,
-            nullable=False,
-            unique=True,
-            index=True)
+    return Column(GUID, server_default=UNIQUE_ID, primary_key=True, nullable=False, unique=True, index=True)
 
 
 def create_foreignKey(table: str, unique: bool = False, index: bool = True, nullable: bool = False):
@@ -40,6 +55,7 @@ def IRAN_TIME(dump: bool = False):
         return str(datetime.now(tz=timezone(offset=timedelta(hours=3, minutes=30))))
     return datetime.now(tz=timezone(offset=timedelta(hours=3, minutes=30)))
 
+
 def Extract_Unique_keyPair(error_message) -> str | Dict:
     error_message = str(error_message)
     match = re.search(r'Key \((.*?)\)=\((.*?)\)', error_message)
@@ -48,34 +64,6 @@ def Extract_Unique_keyPair(error_message) -> str | Dict:
     else:
         return error_message
 
-
-CODES = {
-    "user": "0",
-    "role": "1",
-    "status": "2",
-    "language": "3",
-    "course_type": "4"
-}
-
-
-def Exception_Wrapper(func: Callable):
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        logger.info(f'{func.__name__} - {args} - {kwargs}')
-        try:
-            func(*args, **kwargs)
-        except Exception as Error:
-            return
-            db = kwargs["db"]
-            Cluster = func.__name__
-            db.rollback()
-            if "duplicate key" in Error.__repr__() or "UniqueViolation" in Error.__repr__():
-                logger.warning(f'[ {Cluster} / 409 ]{Error.__class__.__name__}: Record Already Exist: {Extract_Unique_keyPair(Error.args)}', depth=2)
-                return
-            logger.error(f'[ {Cluster} / 500 ]{Error.__class__.__name__}: {Error.args}', depth=2)
-
-    return wrapper()
 
 def Unique_ID(num: int, code: str) -> str:
     return f"{str(num).zfill(8)}-{CODES.get(code, 0).zfill(4)}-4b94-8e27-44833c2b940f"
