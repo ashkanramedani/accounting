748936836306e717fbac4e0788d28c49461fd4e0
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-14 10:49:01 +0330

    bug fix related to archive_deleted_record couldn't be deleted after archive trigger

--- api/models/Triggers.py
+++ api/models/Triggers.py
@@ -1,6 +1,8 @@
 import json
 
-from lib import JSONEncoder
+from sqlalchemy.exc import StatementError
+
+from lib import JSONEncoder, logger
 from models import tables as dbm
 
 from sqlalchemy.engine.base import Connection
@@ -9,17 +11,22 @@ from sqlalchemy.orm.mapper import Mapper
 
 
 def archive_deleted_record(mapper: Mapper, connection: Connection, target):
-    ROW_DATA = target.__dict__
-    ROW_DATA.pop("_sa_instance_state", None)
+    try:
 
-    value = next((key.replace("_pk_id", "") for key in ROW_DATA.keys() if "_pk_id" in key), None)
+        Fields = {}
+        ROW_DATA = target.__dict__
+        ROW_DATA.pop("_sa_instance_state", None)
+        value = next((key for key in ROW_DATA.keys() if "_pk_id" in key), None)
 
-    with sessionmaker(autoflush=False, bind=connection)() as db:
-        BackUp_Row = dbm.Deleted_Records(
-                table=value,
-                deleted_fk_by=ROW_DATA.pop("_Deleted_BY", None),
-                rows_data=json.loads(json.dumps(ROW_DATA, cls=JSONEncoder)))
+        Fields["table"] = value.replace("_pk_id", "")
+        Fields["rows_data"] = json.loads(json.dumps(ROW_DATA, cls=JSONEncoder))
+        if Deleted_by := ROW_DATA.pop("_Deleted_BY", None):
+            Fields["deleted_fk_by"] = Deleted_by
+        with sessionmaker(autoflush=False, bind=connection)() as db:
+            BackUp_Row = dbm.Deleted_Records(**Fields)
+            db.add(BackUp_Row)
+            db.commit()
+            db.refresh(BackUp_Row)
 
-        db.add(BackUp_Row)
-        db.commit()
-        db.refresh(BackUp_Row)
+    except StatementError as SE:
+        logger.error(f'{SE.__class__.__name__}: {SE.__repr__()}')
