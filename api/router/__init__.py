b9d5918777bddd65c341601a5d7f7413659f9dbb
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-14 10:11:54 +0330

    minor changes

--- api/router/__init__.py
+++ api/router/__init__.py
@@ -1,13 +1,14 @@
 from .Course import course_route, course_extension_route, sub_course_route, session_route
 from .Entity import student_route, employee_route
 from .Salary import report_route, salary_policy_route
-from .Site import files, user, post, library
+# from .Site import files, user, post, library
 from .Survey_form import question_route, response_route, survey_route
 from .User_form import *
 from .tools import router as tools_route
 from .dropdown import router as dropdown_route
 from .Template import router as template_route
 from .Status import router as status_route
+from .gateway import *
 
 # routes = [
 #     report_route,
@@ -18,6 +19,7 @@ from .Status import router as status_route
 # ]
 
 routes = [
+    zarinpal_route,
     status_route,
     discount_code_router,
     # dropdown_route
@@ -65,10 +67,6 @@ routes = [
     # survey_route,
 
     # Site Form
-    files,
-    user,
-    post,
-    library
 ]
 
 
