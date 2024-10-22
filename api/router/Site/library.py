6b4da61aa39b1cff2a5de1f27707c8b2140bd341
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-22 20:58:05 +0330

    library and post has been added

--- api/router/Site/library.py
+++ api/router/Site/library.py
@@ -1,10 +1,11 @@
 from typing import Optional, List, Any
+from uuid import UUID
 
 from docutils.nodes import topic
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Response, status, HTTPException
 
 import schemas as sch
-from db import db_library
+import db as dbf
 from models import get_db
 
 # from lib.oauth2 import oauth2_scheme, get_current_user, create_access_token, create_refresh_token
@@ -14,170 +15,50 @@ from models import get_db
 router = APIRouter(prefix='/api/v1/library', tags=['Libraries'])
 
 
-# # create libraries
-# @router.put('')
-# def create_libraries(db=Depends(get_db)):
-#     pass
+@router.post("/add", status_code=status.HTTP_200_OK)
+def add_library(Form: sch.post_library, db=Depends(get_db)):
+    status_code, result = dbf.post_library(db, Form)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
-# get products
-# @router.get("/{library_type}", response_model=List[sch.Library], dependencies=[Depends(RateLimiter(times=10, seconds=2))])
-# def get_libraries_by_library_type(library_type: str, limit:Optional[int]=5, q:Optional[str]="", db=Depends(get_db)):
-#     data = db_library.get_libraries_by_library_type(db, library_type=library_type, limit=limit)
-#     if not data:
-#         return False #raise HTTPException(status_code=404, detail="کتابی یافت نشد - کد ۱")     
-
-#     if q is not None or q != "":
-#         res = []
-#         for i in data:
-#             if str(q).lower() in str(i.library_name).lower():
-#                 res.append(i)
-#         return res 
-#     return data
-
-
-# @router.get('/{topic}')
-# def get_libraries(topic: str="public", limit:Optional[int]=1000, db=Depends(get_db)):
-#     data = db_libraries.get_libraries(db, topic, limit)
-#     res = []
-#     for i in data:
-#         res.append(            
-#             {
-#                 "id": i.id,
-#                 "name": i.name,
-#                 "image": i.image,
-#                 "type": i.type,
-#                 "description": i.description,
-#                 "summer": i.summer,
-#                 "download_count": i.download_count,
-#                 "priority": i.priority,
-#                 "audio_file_link": i.audio_file_link,
-#                 "audio_file_path": i.audio_file_path,
-#                 "aparat_video_id": i.aparat_video_id,
-#                 "aparat_video_code": i.aparat_video_code,
-#                 "video_file_link":  i.video_file_link,
-#                 "video_file_path": i.video_file_path,
-#                 "data_file_link": i.data_file_link,
-#                 "data_file_path": i.data_file_path,
-#                 "create_date": i.create_date,
-#                 "expire_date": i.expire_date
-#             }
-#         )
-#     return res
-
-# @router.get('/{topic}/{uid}')
-# def get_libraries_info(uid: int, topic: Optional[str]="public", db=Depends(get_db)):
-#     i = db_libraries.get_libraries(db, topic, uid)
-#     res =   {
-#         "id": i.id,
-#         "name": i.name,
-#         "image": i.image,
-#         "type": i.type,
-#         "description": i.description,
-#         "summer": i.summer,
-#         "download_count": i.download_count,
-#         "priority": i.priority,
-#         "audio_file_link": i.audio_file_link,
-#         "audio_file_path": i.audio_file_path,
-#         "aparat_video_id": i.aparat_video_id,
-#         "aparat_video_code": i.aparat_video_code,
-#         "video_file_link":  i.video_file_link,
-#         "video_file_path": i.video_file_path,
-#         "data_file_link": i.data_file_link,
-#         "data_file_path": i.data_file_path,
-#         "create_date": i.create_date,
-#         "expire_date": i.expire_date
-#     }
-#     return res
 
 # read topics
 @router.get('/get_library_topic', response_model=List[dict], status_code=status.HTTP_200_OK)
 def get_library_topic() -> List:
     topics = [
-        {
-            "topic_name": "magazine",
-            "label_name": "مجلات"
-        },
-        {
-            "topic_name": "story",
-            "label_name": "داستانی"
-        },
-        {
-            "topic_name": "educational",
-            "label_name": "آموزشی"
-        }
+        {"topic_name": "magazine", "label_name": "مجلات"},
+        {"topic_name": "story", "label_name": "داستانی"},
+        {"topic_name": "educational", "label_name": "آموزشی"}
     ]
     return topics
 
 
-@router.get('/{topic}/read', response_model=List[sch.Library], status_code=status.HTTP_200_OK)
-def get_all_library_with_limit(topic: str, start_id: Optional[int] = 0, page_number: Optional[int] = 0, limit: Optional[int] = 1000, db=Depends(get_db)) -> Any:
-    data = db_library.read_all_library_for_admin_panel(db, topic, start_id, page_number, limit)
-
-    if data == -1:
-        return Response(status_code=500, content="خطایی رخ داده است")
-
-    if not data or data is None or data == [] or data is False:
-        return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-    return data
+@router.get('/{topic}/read', status_code=status.HTTP_200_OK)
+def get_all_library_with_limit(topic: str, page_number: Optional[int] = 0, limit: Optional[int] = 1000, db=Depends(get_db)) -> Any:
+    status_code, result = dbf.read_all_library_for_admin_panel(db, topic, page_number, limit)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
 
-@router.get('/{topic}/{pid}', response_model=sch.Library)
+@router.get('/{topic}/{pid}')
 def get_library_with_pid(topic: str, pid: str, db=Depends(get_db)):
-    data = db_library.get_library_with_pid(db, pid)
-
-    if data == -1:
-        return Response(status_code=500, content="خطایی رخ داده است")
-
-    if not data or data is None or data == [] or data is False:
-        return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-    return data
-
-
-# create libraries
-@router.post('', status_code=201)
-def create_library(db=Depends(get_db)):
-    pass
-
-
-# update libraries
-@router.patch('/update/{lid}')
-def update_library(lid: str, db=Depends(get_db)):
-    pass
-
-
-@router.patch('/group-partial-modifications-in-group-library')
-def group_partial_modifications_in_group_library(db=Depends(get_db)):
-    pass
-
-
-@router.patch('/group-status-modifications-in-group-library')
-def group_status_modifications_in_group_library(db=Depends(get_db)):
-    pass
+    status_code, result = dbf.get_library_with_pid(db, pid)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
 
 # delete libraries
 @router.delete('/delete/{pid}', status_code=status.HTTP_200_OK)
-def delete_library(pid: str, db=Depends(get_db)):
-    res = db_library.delete_posts(db, topic, pid)
-    if res == 1:
-        return Response(status_code=200, content="داده با موفقیت حذف شد")
-
-    elif res == 0:
-        return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-    elif res == -1:
-        return Response(status_code=500, content="خطایی رخ داده است")
+def delete_library(pid: UUID, db=Depends(get_db)):
+    status_code, result = dbf.delete_posts(db, pid)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
 
 @router.delete('/group-delete', status_code=status.HTTP_200_OK)
-def delete_group_library(list_post: List[sch.LibraryDelete], db=Depends(get_db)) -> Any:
-    res = None
-    for i in list_post:
-        if res is None:
-            res = {}
-        if i not in res:
-            res[i] = db_library.delete_libraries(db, topic, i)
-
-    return res
+def delete_group_library(list_post: List[UUID], db=Depends(get_db)):
+    return {str(ID): dbf.delete_libraries(db, topic) for ID in list_post}
--- api/router/Site/post.py
+++ api/router/Site/post.py
@@ -1,10 +1,11 @@
 from typing import Optional, List, Any
+from uuid import UUID
 
 from docutils.nodes import topic
-from fastapi import APIRouter, Depends, Response, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 
+import db as dbf
 import schemas as sch
-from db import db_post
 from models import get_db
 
 # from lib.oauth2 import oauth2_scheme, get_current_user, create_access_token, create_refresh_token
@@ -21,160 +22,47 @@ router = APIRouter(prefix='/api/v1/post', tags=['Post'])
 @router.get('/get_post_topic', response_model=List[dict], status_code=status.HTTP_200_OK)
 def get_post_topic() -> List:
     topics = [
-        {
-            "topic_name": "news",
-            "label_name": "اخبار"
-        },
-        {
-            "topic_name": "blog",
-            "label_name": "بلاگ"
-        },
-        {
-            "topic_name": "podcast",
-            "label_name": "پادکست"
-        }
+        {"topic_name": "news", "label_name": "اخبار"},
+        {"topic_name": "blog", "label_name": "بلاگ"},
+        {"topic_name": "podcast", "label_name": "پادکست"}
     ]
     return topics
 
 
 # create posts
-@router.post('', status_code=201)
-def create_a_post(post: sch.PostCreate, db=Depends(get_db)) -> Any:
-    data = None
-
-    data = db_post.create_post(db, post)
-
-    if data is None or data is False:
-        raise HTTPException(status_code=500, detail="خطایی رخ داده است")
-
-    return data
-
-
-# update posts
-@router.put('/{topic}', status_code=status.HTTP_200_OK)
-def partial_modifications_all_post_in_topic(topic: str, post: sch.PostCreate, db=Depends(get_db)) -> Any:
-    return {"detail": "در دست ساخت است"}
-
-
-# update posts
-@router.patch('/update/{pid}', status_code=status.HTTP_200_OK)
-def partial_modifications_in_a_post(topic: str, post: sch.PostCreate, pid: str, db=Depends(get_db)) -> Any:
-    return {"detail": "در دست ساخت است"}
-
-
-@router.patch('/group-partial-modifications-in-group-post', status_code=status.HTTP_200_OK)
-def group_partial_modifications_in_group_post(list_post: List[sch.PostCreate], db=Depends(get_db)) -> Any:
-    return {"detail": "در دست ساخت است"}
-
-
-@router.patch('/group-status-modifications-in-group-post', status_code=status.HTTP_200_OK)
-def group_status_modifications_in_group_post(list_post: List[sch.PostStatus], db=Depends(get_db)) -> Any:
-    return {"detail": "در دست ساخت است"}
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_a_post(Form: sch.create_post, db=Depends(get_db)):
+    status_code, result = dbf.create_post(db, Form)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
 
 # delete posts
 @router.delete('/delete/{pid}', status_code=status.HTTP_200_OK)
-def delete_post(pid: int, db=Depends(get_db)) -> Any:
-    res = db_post.delete_posts(db, topic, pid)
-    if res == 1:
-        return Response(status_code=200, content="داده با موفقیت حذف شد")
-
-    elif res == 0:
-        return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-    elif res == -1:
-        return Response(status_code=500, content="خطایی رخ داده است")
+def delete_post(pid: UUID, db=Depends(get_db)) -> Any:
+    status_code, result = dbf.delete_posts(db, topic, pid)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
 
 @router.delete('/group-delete', status_code=status.HTTP_200_OK)
-def group_delete_post(list_post: List[sch.PostDelete], db=Depends(get_db)) -> Any:
-    res = None
-    for i in list_post:
-        if res is None:
-            res = {}
-        if i not in res:
-            res[i] = db_post.delete_posts(db, topic, i)
-
-    return res
+def group_delete_post(posts: List[UUID], db=Depends(get_db)):
+    return {str(ID): dbf.delete_posts(db, topic, ID) for ID in posts}
 
 
-# read post - get posts all posts with limit
 @router.get('/{topic}/read', response_model=List[sch.Posts], status_code=status.HTTP_200_OK)
-def get_all_posts_with_limit(topic: str, start_id: Optional[int] = 0, page_number: Optional[int] = 0, limit: Optional[int] = 1000, db=Depends(get_db)) -> Any:
-    data = db_post.read_all_posts_for_admin_panel(db, topic, start_id, page_number, limit)
-
-    if data == -1:
-        return Response(status_code=500, content="خطایی رخ داده است")
-
-    if not data or data is None or data == [] or data is False:
-        return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-    return data
+def get_all_posts_with_limit(topic: str, page_number: Optional[int] = 0, limit: Optional[int] = 1000, db=Depends(get_db)):
+    status_code, result = dbf.read_all_posts_for_admin_panel(db, topic, page_number, limit)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
 
 
 @router.get('/{topic}/{pid}', response_model=sch.Posts)
-def get_post_with_pid(topic: str, pid: str, db=Depends(get_db)):
-    data = db_post.get_post_with_pid(db, pid)
-
-    if data == -1:
-        return Response(status_code=500, content="خطایی رخ داده است")
-
-    if not data or data is None or data == [] or data is False:
-        return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-    return data
-
-# get posts with search posts with limit
-# @router.get('/{topic}/search', response_model=List[sch.Posts], status_code=status.HTTP_200_OK)
-# def get_all_posts_with_limit_with_search(q:Optional[str], topic:str, limit:Optional[int]=1000, db=Depends(get_db)):
-#     res = []
-#     data = db_post.get_all_posts_with_limit(db, topic, limit)
-#     for i in data:
-#         if i.post_title is not None and str(q).lower() in str(i.post_title).lower():
-#             res.append(i) 
-#         elif i.post_Summary is not None and str(q).lower() in str(i.post_Summary).lower():
-#             res.append(i) 
-#         elif i.post_discribtion is not None and str(q).lower() in str(i.post_discribtion).lower():
-#             res.append(i) 
-#         elif i.post_content is not None and str(q).lower() in str(i.post_content).lower():
-#             res.append(i)    
-#     for i in res:
-#         viwe = db_post.get_viwe(db, i.post_pk_id)
-#         i.viwe = viwe
-#     return res
-
-# # get posts all posts with page and limit
-# @router.get('/{topic}/', response_model=List[sch.Posts])
-# def get_all_posts_with_page_and_limit(topic:str, page:Optional[int]=0, limit:Optional[int]=9, db=Depends(get_db)) -> Any:
-#     data = db_post.get_all_posts_with_page_and_limit(db, topic, limit, page)
-#     for i in data:
-#         viwe = db_post.get_viwe(db, i.post_pk_id)
-#         i.viwe = viwe
-#     return data
-
-# # get posts one post
-# @router.get('/{topic}', response_model=sch.Post)
-# def get_one_post(topic:str, id:int, db=Depends(get_db)) -> Any:
-#     data = db_post.get_post(db, topic, id)
-#     viwe = db_post.get_viwe(db, id)
-#     data.viwe = viwe
-#     return data
-
-# # get posts one post
-# @router.put('/{topic}/viwe')
-# def get_one_post(topic:str, data:sch.PostViwesCreate, db=Depends(get_db)) -> Any:
-#     resviwe = db_post.put_viwe(db, data)
-#     return True
-
-
-# @router.put('/{topic}/update/{pid}', status_code=status.HTTP_200_OK)
-# def update_post(topic:str, pid:int, update:sch.PostUpdateData, db=Depends(get_db)) -> Any:
-#     res = db_posts.update_posts(db, topic, pid, update)   
-#     if res == 1:
-#         return Response(status_code=200, content="اطلاعات با موفقیت بروز رسانی شد")
-
-#     elif res == 0:
-#         return Response(status_code=404, content="هیچ داده‌ای یافت نشد")
-
-#     elif res == -1:
-#         return Response(status_code=500, content="خطایی رخ داده است")
+def get_post_with_pid(pid: UUID, db=Depends(get_db)):
+    status_code, result = dbf.get_post_with_pid(db, pid)
+    if status_code not in sch.SUCCESS_STATUS:
+        raise HTTPException(status_code=status_code, detail=result)
+    return result
