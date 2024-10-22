420b004f750387d0fafae2a1094d43a8558ce1ae
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-22 20:57:03 +0330

    minor update

--- api/router/Site/__init__.py
+++ api/router/Site/__init__.py
@@ -1,4 +1,2 @@
-from .files import router as files
-from .library import router as library
-from .post import router as post
-from .user import router as user
+# from .library import router as library
+# from .post import router as post
