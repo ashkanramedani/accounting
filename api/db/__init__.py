7121f3481e736d566b77a6eaea64b1f361f14e13
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-22 21:00:32 +0330

    minor changes

--- api/db/__init__.py
+++ api/db/__init__.py
@@ -2,8 +2,9 @@ from .Course import *
 from .Entity import *
 from .Extra import *
 from .Salary import *
-# from .Site import *
-from .Survey_form import *
 from .User_Form import *
-from .product_mapping import *
 from .gateway import *
+
+from .product import *
+# from .Survey_form import *
+# from .product_mapping import *
