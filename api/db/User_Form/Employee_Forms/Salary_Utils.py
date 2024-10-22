8ec7f900912a9df6a9bcf2505e14b96470070c2d
Hsa77 <51709689+Hsa77@users.noreply.github.com>
2024-10-22 21:00:22 +0330

    major improvement in salary calculation

--- api/db/User_Form/Employee_Forms/Salary_Utils.py
+++ api/db/User_Form/Employee_Forms/Salary_Utils.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, date, time, datetime
-from typing import List, Dict
+from typing import List, Dict, Literal, Tuple
 
 from db import Return_Exception
 import models as dbm
@@ -17,23 +17,28 @@ def calculate_duration(time_1: time | None, time_2: time | None):
 
 
 @DEV_io()
-def Calculate_income(salary_rate: dbm.Salary_Policy_form, **Total_activity):
+def Calculate_income(salary_rate: dbm.Salary_Policy_form, Total_activity: Dict, Total_Holiday: int):
     Base_Salary = salary_rate.Base_salary
-    earning = {
-        "Regular_earning": Base_Salary * salary_rate.Regular_hours_factor * Total_activity.get("Regular_hours", 0),
-        "Overtime_earning": Base_Salary * salary_rate.overtime_factor * Total_activity.get("Overtime", 0),
-        "Off_Day_earning": Base_Salary * salary_rate.off_day_factor * Total_activity.get("Off_Day", 0),
-        "remote_earning": Base_Salary * salary_rate.remote_factor * Total_activity.get("remote", 0),
-        "vacation_leave_earning": Base_Salary * salary_rate.vacation_leave_factor * (salary_rate.vacation_leave_cap - Total_activity.get("vacation_leave", 0)),
-        "medical_leave_earning": Base_Salary * salary_rate.medical_leave_factor * (min(0, salary_rate.medical_leave_cap - Total_activity.get("medical_leave", 0))),
-        "business_trip_earning": Base_Salary * salary_rate.business_trip_factor * Total_activity.get("business_trip", 0),
-        "rewards_earning": 0,
-        "Fix_pay": salary_rate.Fix_pay
+
+    Salary_on_Holiday = Total_Holiday * salary_rate.Regular_hours_cap
+    total_off_day = Total_activity["Off_Day"] if salary_rate.off_day_permission else 0
+
+    earning: Dict[str, float] = {
+        "Regular_earning": salary_rate.Regular_hours_factor * Total_activity["Regular_hours"],
+        "Overtime_earning": salary_rate.overtime_factor * Total_activity["Overtime"] if salary_rate.overtime_permission else 0,
+        "Off_Day_earning": (min(0, Salary_on_Holiday - total_off_day) * salary_rate.Regular_hours_factor) + (total_off_day * salary_rate.off_day_factor),
+        "remote_earning": salary_rate.remote_factor * Total_activity["remote"] if salary_rate.remote_permission else 0,
+        "business_trip_earning": salary_rate.business_trip_factor * Total_activity["business_trip"] if salary_rate.business_trip_permission else 0,
+        "vacation_leave_earning": salary_rate.vacation_leave_factor * (salary_rate.vacation_leave_cap - Total_activity["vacation_leave"]),
+        "medical_leave_earning": salary_rate.medical_leave_factor * (min(0, salary_rate.medical_leave_cap - Total_activity["medical_leave"]))
     }
+
+    earning = {k: v * Base_Salary for k, v in earning.items()}
+    earning |= {"rewards_earning": 0, "Fix_pay": salary_rate.Fix_pay}
 
     # Should be all Positive
     deduction = {
-        "Undertime_deductions": Base_Salary * salary_rate.undertime_factor * Total_activity.get("Undertime", 0),
+        "Undertime_deductions": Base_Salary * salary_rate.undertime_factor * Total_activity["Undertime"],
         "insurance_deductions": 0,
         "tax_deductions": 0,
         "punishment_deductions": 0
@@ -48,11 +53,14 @@ def Calculate_income(salary_rate: dbm.Salary_Policy_form, **Total_activity):
 
 
 @DEV_io()
-def Sum_of_Activity(salary_rate, Day_activity: List) -> Dict[str, int]:
+def Sum_of_Activity(salary_rate: dbm.Salary_Policy_form, Day_activity: List[Dict]) -> Dict[str, int]:
     caps = {
         "Overtime": salary_rate.overtime_cap,
-        "Off_Day_earning": salary_rate.off_day_cap
-    }
+        "Off_Day": salary_rate.off_day_cap,
+        "remote": salary_rate.remote_cap,
+        "vacation_leave": salary_rate.vacation_leave_cap,
+        "medical_leave": salary_rate.medical_leave_cap,
+        "business_trip": salary_rate.business_trip_cap}
 
     rates = {
         key: min(sum(day[key] for day in Day_activity), caps.get(key, INF))
@@ -77,6 +85,7 @@ def Date_constructor(Date_obj: str | date | datetime):
 
 
 def Create_Day_Schema(Date: str | date | datetime, Activities: Dict, message="Created") -> Dict:
+    activities = {field: Activities[field].get(str(Date), 0) for field in ["remote", "vacation_leave", "medical_leave", "business_trip"]}
     return {
         "Date": Date_constructor(Date),
         "Holiday": is_off_day(Fix_date(Date)),
@@ -90,12 +99,9 @@ def Create_Day_Schema(Date: str | date | datetime, Activities: Dict, message="Cr
         "delay": 0,
         "haste": 0,
         "attendance_points": 0,
-        "remote": Activities["remote"].get(str(Date), 0),
-        "vacation_leave": Activities["vacation_leave"].get(str(Date), 0),
-        "medical_leave": Activities["medical_leave"].get(str(Date), 0),
-        "business_trip": Activities["business_trip"].get(str(Date), 0),
         "EnterExit": [],
-        "msg": message}
+        "msg": message,
+        **activities}
 
 
 @DEV_io()
@@ -129,14 +135,24 @@ def preprocess_report(report, Activities: Dict):
     return Days
 
 
+def Accrued_Holiday(Day):
+    Day["msg"] = "Accrued_Holiday"
+    return Day
+
+def finalize(Day: Dict) -> Dict:
+    Day["Undertime"] = max(0, Day["Undertime"] - sum(Day[field] for field in ["remote", "vacation_leave", "medical_leave", "business_trip"]))
+    Day["EnterExit"] = ' '.join([str(t) for t in Day.pop("EnterExit", [])])
+    Day["msg"] = "Finished"
+    return Day
+
 @DEV_io()
-def Fixed_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List[Dict]:
+def Fixed_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days: Dict[str, Dict]) -> Tuple[List[Dict], int]:
+    Total_Holiday = 0
     Days = []
 
     for Date, Day_OBJ in preprocess_Days.items():
         if Day_OBJ["Accrued_Holiday"]:
-            Day_OBJ["msg"] = "Accrued_Holiday"
-            Days.append(Day_OBJ)
+            Days.append(Accrued_Holiday(Day_OBJ))
             continue
 
         if not Day_OBJ["IsValid"]:
@@ -145,9 +161,8 @@ def Fixed_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List[
 
         # present on Holiday
         if Day_OBJ["Holiday"]:
-            if EMP_Salary.off_day_permission:
-                Day_OBJ["Off_Day"] = Day_OBJ["present_time"]
-                Day_OBJ["Regular_hours"] = max(0, EMP_Salary.Regular_hours_cap - Day_OBJ["present_time"])
+            Total_Holiday += 1
+            Day_OBJ["Off_Day"] = Day_OBJ["present_time"]
 
         # Not Present on working day
         elif not Day_OBJ["EnterExit"]:
@@ -187,28 +202,25 @@ def Fixed_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List[
 
             # check if more than one Enter and Exit is in day
             if WorkingHours:
-                for Enter, Exit in zip(WorkingHours[:-1], WorkingHours[1:]):
+                for Enter, Exit in zip(WorkingHours[:-1:2], WorkingHours[1::2]):
                     Day_OBJ["Regular_hours"] += time_gap(Enter, Exit)
 
             Day_OBJ["Undertime"] = EMP_Salary.Regular_hours_cap - Day_OBJ["Regular_hours"]
 
             possible_overtime = 0
             if NonWorkingHours:
-                for Enter, Exit in zip(NonWorkingHours[:-1], NonWorkingHours[1:]):
+                for Enter, Exit in zip(NonWorkingHours[:-1:2], NonWorkingHours[1::2]):
                     possible_overtime += time_gap(Enter, Exit)
 
             if possible_overtime > EMP_Salary.overtime_threshold:
                 Day_OBJ["Overtime"] = possible_overtime
 
-        Day_OBJ["Undertime"] = max(0, Day_OBJ["Undertime"] - (Day_OBJ["remote"] + Day_OBJ["vacation_leave"] + Day_OBJ["medical_leave"] + Day_OBJ["business_trip"]))
-
-        Day_OBJ["EnterExit"] = ' '.join([str(t) for t in Day_OBJ.pop("EnterExit", [])])
-        Day_OBJ["msg"] = "Finished"
-        Days.append(Day_OBJ)
-    return Days
+        Days.append(finalize(Day_OBJ))
+    return Days, Total_Holiday
 
 
-def Split_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List[Dict]:
+def Split_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> Tuple[List[Dict], int]:
+    Total_Holiday = 0
     Days = []
 
     for Date, Day_OBJ in preprocess_Days.items():
@@ -222,33 +234,28 @@ def Split_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List[
             continue
 
         if Day_OBJ["Holiday"]:
-            if EMP_Salary.off_day_permission:
-                Day_OBJ["Off_Day"] = Day_OBJ["present_time"]
-                Day_OBJ["Regular_hours"] = max(0, EMP_Salary.Regular_hours_cap - Day_OBJ["present_time"])
+            Total_Holiday += 1
+            Day_OBJ["Off_Day"] = Day_OBJ["present_time"]
         else:
-            if Day_OBJ["present_time"] >= EMP_Salary.Regular_hours_cap:
-                possible_Overtime = Day_OBJ["present_time"] - EMP_Salary.Regular_hours_cap
+            Regular_hours_cap = EMP_Salary.Regular_hours_cap
+            if Day_OBJ["present_time"] >= Regular_hours_cap:
+                possible_Overtime = Day_OBJ["present_time"] - Regular_hours_cap
                 Day_OBJ["Overtime"] = possible_Overtime if possible_Overtime >= EMP_Salary.overtime_threshold else 0
             else:
-                possible_undertime = EMP_Salary.Regular_hours_cap - Day_OBJ["present_time"]
+                possible_undertime = Regular_hours_cap - Day_OBJ["present_time"]
                 Day_OBJ["Undertime"] = possible_undertime if possible_undertime >= EMP_Salary.undertime_threshold else 0
-            Day_OBJ["Regular_hours"] = min(EMP_Salary.Regular_hours_cap, Day_OBJ["present_time"])
+            Day_OBJ["Regular_hours"] = min(Regular_hours_cap, Day_OBJ["present_time"])
 
-            Day_OBJ["msg"] = "Finished"
-
-        Day_OBJ["Undertime"] = max(0, Day_OBJ["Undertime"] - (Day_OBJ["remote"] + Day_OBJ["vacation_leave"] + Day_OBJ["medical_leave"] + Day_OBJ["business_trip"]))
-        Day_OBJ["EnterExit"] = ' '.join([str(t) for t in Day_OBJ.pop("EnterExit", [])])
-        Days.append(Day_OBJ)
-    return Days
+        Days.append(finalize(Day_OBJ))
+    return Days,Total_Holiday
 
 
 @DEV_io()
-def Hourly_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List[Dict]:
+def Hourly_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> Tuple[List[Dict], int]:
     Days = []
     for Date, Day_OBJ in preprocess_Days.items():
         if Day_OBJ["Accrued_Holiday"]:
-            Day_OBJ["msg"] = "Accrued_Holiday"
-            Days.append(Day_OBJ)
+            Days.append(Accrued_Holiday(Day_OBJ))
             continue
 
         if not Day_OBJ["IsValid"]:
@@ -256,8 +263,8 @@ def Hourly_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List
             continue
 
         if Day_OBJ["Holiday"]:
-            if EMP_Salary.off_day_permission:
-                Day_OBJ["Off_Day"] = Day_OBJ["present_time"]
+            Day_OBJ["Off_Day"] = Day_OBJ["present_time"]
+
         else:  # NC: 005
             if Day_OBJ["present_time"] >= EMP_Salary.Regular_hours_cap:
                 possible_Overtime = Day_OBJ["present_time"] - EMP_Salary.Regular_hours_cap
@@ -268,12 +275,8 @@ def Hourly_schedule(EMP_Salary: dbm.Salary_Policy_form, preprocess_Days) -> List
                 Day_OBJ["attendance_points"] -= 1
 
             Day_OBJ["Regular_hours"] = min(EMP_Salary.Regular_hours_cap, Day_OBJ["present_time"])
-
-        Day_OBJ["Undertime"] = max(0, Day_OBJ["Undertime"] - (Day_OBJ["remote"] + Day_OBJ["vacation_leave"] + Day_OBJ["medical_leave"] + Day_OBJ["business_trip"]))
-        Day_OBJ["EnterExit"] = ' '.join([str(t) for t in Day_OBJ.pop("EnterExit", [])])
-        Day_OBJ["msg"] = "Finished"
-        Days.append(Day_OBJ)
-    return Days
+        Days.append(finalize(Day_OBJ))
+    return Days, 0
 
 
 @DEV_io()
@@ -282,24 +285,21 @@ def generate_daily_report(Salary_Policy: dbm.Salary_Policy_form, Fingerprint_sca
     Generate the daily report Base on Employee fingerprint scanner report
     """
     try:
-
-        final_result = {}
-        report_dicts = preprocess_report(Fingerprint_scanner_report, Activities)
+        report_dicts: Dict[str, Dict] = preprocess_report(Fingerprint_scanner_report, Activities)
 
         # Split schedule and Fix schedule
         if Salary_Policy.Salary_Type == "Fixed":
-            final_result["Days"]: List[dict] = Fixed_schedule(Salary_Policy, report_dicts)
+            Days, Total_Holiday = Fixed_schedule(Salary_Policy, report_dicts)
         elif Salary_Policy.Salary_Type == "Split":
-            final_result["Days"]: List[dict] = Split_schedule(Salary_Policy, report_dicts)
+            Days, Total_Holiday = Split_schedule(Salary_Policy, report_dicts)
         elif Salary_Policy.Salary_Type == "Hourly":
-            final_result["Days"]: List[dict] = Hourly_schedule(Salary_Policy, report_dicts)
+            Days, Total_Holiday = Hourly_schedule(Salary_Policy, report_dicts)
         else:
             return 400, "Invalid Salary Type"
 
-        Total_Activity = Sum_of_Activity(Salary_Policy, final_result["Days"])
-        final_result |= Total_Activity
-        final_result |= Calculate_income(Salary_Policy, **Total_Activity)
+        Total_Activity = Sum_of_Activity(Salary_Policy, Days)
+        income = Calculate_income(Salary_Policy, Total_Activity, Total_Holiday)
 
-        return 200, final_result
+        return 200, {"Days": Days, "Total_Holiday": Total_Holiday, **Total_Activity, **income}
     except Exception as e:
         return Return_Exception(Error=e)
